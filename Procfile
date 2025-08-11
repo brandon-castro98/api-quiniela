bc622ac63e8c1af658c0bed7691446990cd5b73d
brandon-castro98 <jonathan_works98@outlook.com>
2025-08-11 09:45:09 -0600

    "Updated files for railway"

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn quinielas_backend.wsgi
--- quinielas_backend/settings.py
+++ quinielas_backend/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%@$+vykgf8ar21v7kz_*z*^54frnxrxcfjawq^cc%d5i%-6#&4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
