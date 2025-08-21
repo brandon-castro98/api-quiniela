8b92cdd75360ca95820b32d2f133eab675f1854f
brandon-castro98 <jonathan_works98@outlook.com>
2025-08-21 05:15:51 -0600

    "Config of settings"

--- quinielas_backend/settings.py
+++ quinielas_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='kvhkjae4JrEsIxoOY4B/jQQ8Lb8yijZHvp2RpuYG2hh6tc+2Gl/NgLDezTZQjBtL')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*','https://api-quiniela-phiv.onrender.com']
 
